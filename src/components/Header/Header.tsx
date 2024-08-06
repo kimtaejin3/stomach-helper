2f45b0f8882444ef246254e67cdec93b5f0091bd
kimtaejin3 <rlaxowls1316@naver.com>
2024-08-06 14:18:47 +0900

    logo에 link 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.css";
+import { GiStomach } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +9,11 @@ export default function Header() {
   return (
     <header className={cx("header")}>
       <div className="layout_wrapper">
-        <div className="headings">Header</div>
+        <div className="headings">
+          <Link to="#">
+            <GiStomach size={30} color="A9A4EE" />
+          </Link>
+        </div>
       </div>
     </header>
   );
