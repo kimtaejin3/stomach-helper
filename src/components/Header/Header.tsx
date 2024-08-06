f49647a2a0ae782b3210e495fca70b2da9964e63
kimtaejin3 <rlaxowls1316@naver.com>
2024-08-06 14:05:17 +0900

    classnames 설치

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className="layout_wrapper">
+        <div className="headings">Header</div>
+      </div>
+    </header>
+  );
+}
